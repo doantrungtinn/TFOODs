0e6b1ae48bef72a886c7a8c01ad6cdadb8d29b4e
doantrungtinn <doantrungtinn@gmail.com>
2021-09-03 20:39:04 +0700

    abc

---
+++ src/BUS/ProductBus.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BUS;
+
+import DAO.ProductDao;
+import DTO.Product;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author tindoan
+ */
+public class ProductBus {
+    ProductDao ProductDao = new ProductDao();
+    public boolean insertProduct(Product p) throws Exception{
+            if(ProductDao.insertProduct(p)){
+                return true;
+            }
+         return false;
+        }  
+     public ArrayList<Product>getAllRows(int id)throws Exception{
+         return ProductDao.getAllRows();
+     }
+     
+     public ArrayList<Product> searchProduct(String key) throws SQLException{
+         return ProductDao.searchProduct(key);
+     }
+      public Product getbyId(int id){
+          return ProductDao.getbyId(id);
+      }
+   public int updateProduct(Product product){
+       return ProductDao.updateProduct(product);
+   }
+}
+
+
